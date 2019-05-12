51f3635907491740852d1da6f70655c68b6acda7
Egors01 <Egor.Semenchenko@Skoltech.ru>
2019-05-12 15:21:42 +0300

    FIXED PATHS

--- deconstruct_sigs/helper.py
+++ deconstruct_sigs/helper.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
-
+import os
 class SigHelper(object):
     def __init__(self):
+        package_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), os.path.pardir)
 
         self.ordered_signatures_sample = pd.read_csv(
-        '/home/egors/deconstruct_sigs/data/signatures_probabilities.txt',
+        os.path.join(package_path,'data/signatures_probabilities.txt'),
         sep='\t', usecols=np.arange(0, 33))
 
         self.input_signatures_sample = pd.read_csv(
-        '/home/egors/deconstruct_sigs/data/signatures_probabilities.txt',
+        os.path.join(package_path,'data/signatures_probabilities.txt'),
         sep='\t', usecols=np.arange(3, 33))
 
         temdf = pd.DataFrame()
@@ -28,3 +30,4 @@ class SigHelper(object):
     def get_annotation_sample(self):
         return self.annotation_sample
 
+SigHelper()
